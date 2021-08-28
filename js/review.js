26e25f32e8de744b90daa811b08b38047b859dbc
IFC2021 <88974538+IFC2021@users.noreply.github.com>
2021-08-28 22:25:08 +0530

    Update review.js

--- js/review.js
+++ js/review.js
@@ -25,6 +25,7 @@ var productVariantsToRange = "F100";
 var readProductVariantsURL = sheetAPIBaseURL + "/" + ExcelfileID + "/values/" + productVariantsSheetName + "!" + productVariantsFromRange + ":" + productVariantsToRange + "?key=" + apiKey;
 /*Read products ends*/
 
+
 var categoryResult = null;
 var productResult = null;
 var productVariantsResult = null;
@@ -51,7 +52,7 @@ $(document).ready(function () {
     else {
         loadReviewCart();
     }
-	updateCartCount();
+    updateCartCount();
 });
 
 function getCategoriesAjax() {
@@ -103,20 +104,19 @@ function getProductVariantsAjax() {
 }
 
 function loadMenuCategories(categories) {
-    var catPart1 = "<li><a href='products.html'>All Categories</a></li>";
+    var catPart1 = '<li><a href="javascript:" onclick="navigateToProducts(\'All\')" >All Categories</a></li>';
     var catPart2 = '';
     var partition = Math.round((categories.length / 2));
 
     for (var i = 0; i < partition; i++) {
-        catPart1 += "<li><a href='products.html?category=" + categories[i] + "'>" + categories[i] + "</a></li>";
+        catPart1 += '<li><a href="javascript:" onclick="navigateToProducts(\'' + categories[i] + '\')"> ' + categories[i] + '</a ></li >';
     }
     for (var j = partition; j < categories.length; j++) {
-        catPart2 += "<li><a href='products.html?category=" + categories[j] + "'>" + categories[j] + "</a></li>";
+        catPart2 += '<li><a href="javascript:" onclick="navigateToProducts(\'' + categories[j] + '\')"> ' + categories[j] + '</a ></li >';
     }
     $('#catPart1').html(catPart1);
     $('#catPart2').html(catPart2);
 }
-
 function loadSearchCategories(categories) {
     var ddlOptions = "<option value=''>All Categories</option>";
     for (var i = 0; i < categories.length; i++) {
@@ -124,20 +124,18 @@ function loadSearchCategories(categories) {
     }
     $('#searchCat').html(ddlOptions);
 }
-
 function loadMobileViewMenuCat(categories) {
-    var mobileViewMenuCat = '';
+    var mobileViewMenuCat = '<li><a href="javascript:" onclick="navigateToProducts(\'All\')" >All Categories</a></li>';
     for (var i = 0; i < categories.length; i++) {
         var encodedURL = encodeURIComponent(categories[i]);
-        mobileViewMenuCat += "<li><a href='products.html?category=" + encodedURL + "'>" + categories[i] + "</a></li>";
+        mobileViewMenuCat += '<li><a href="javascript:" onclick="navigateToProducts(\'' + categories[i] + '\')">' + categories[i] + '</a></li>';
         $('#mobileViewMenuCat').html(mobileViewMenuCat);
     }
 }
-
 function loadReviewCart() {
     var cartObj = [];
 
-    if (localStorage.getItem("cart") != null && localStorage.getItem("cart") != '') {
+    if (localStorage.getItem("cart") != null && localStorage.getItem("cart") != '' && localStorage.getItem("cart") != "[]") {
         cartObj = JSON.parse(localStorage.getItem("cart"));
 
         var cartItemBlock = '';
@@ -146,27 +144,28 @@ function loadReviewCart() {
             var product = productResult.filter(function (obj) {
                 return (obj[1] == cartObj[i].ProductID);
             });
-            cartItemBlock += '<tr><td class="product-col"><figure class="product-image-container"><a href="javascript:" class="product-image"> <img id="reviewProductImage" src="ProductImages/' + product[0][3] + '" alt="product"> <input type="hidden" id="reviewProductID" /> </a> </figure> <div class="widget widget-categories"> <h4 class="widget-title">' + product[0][2] + '</h4> <ul class="list">@@VariantOptions</div> </td>  <td class="price-col">Quanitity: <span class="">' + cartObj[i].Quantity + '</span></td>  </tr>';
+            cartItemBlock += '<tr><td class="product-col"><figure class="product-image-container"><a href="javascript:" class="product-image"> <img id="reviewProductImage" src="ProductImages/' + product[0][3] + '" alt="product"> <input type="hidden" id="reviewProductID" /> </a> </figure> <div class="widget widget-categories"> <h4 class="widget-title">' + product[0][2] + '</h4> <ul class="list">@@VariantOptions</div> </td>  <td class="price-col">Quanitity: <span class="">' + cartObj[i].Quantity + '</span></td><td style="vertical-align:bottom"><button onclick="editCart(\'' + product[0][1] + '\')" class="btn btn-xs-edit btn-info" type="button"><i class="fa fa-edit"></i></button><br/><button onclick="deleteCartRow(\'' + cartObj[i].CartRowIndex + '\')" class="btn btn-xs-delete btn-danger" type="button"><i class="fa fa-trash"></i></button></td></tr>';
 
-            var variantList = $.map(cartObj[i], function (value, key) {
+            var variantList = $.map(cartObj[i].cartItemVariant, function (value, key) {
                 return [[key, value]];
             });
-            //variantList = variantList.slice(2);
+
             var variantBlock = '';
             for (var j = 0; j < variantList.length; j++) {
 
+                var currentVariant = productVariantsResult.filter(function (obj) {
+                    return (obj[2] == variantList[j][1] && obj[0] == cartObj[i].ProductID);
+                });
+
                 if (variantList[j][0] == "Color") {
-                    variantBlock += '<li><a href="javascript:">Color: <div style="background-color: ' + variantList[j][1] + '; height: 20px; width: 20px; display: inline-block; margin-bottom: -5px;"></div></a> </li>';
+
+                    variantBlock += '<li><a href="javascript:">Color:  <span class="">' + currentVariant[0][4] + '</span> | <div style="background-color: ' + variantList[j][1] + '; height: 20px; width: 20px; display: inline-block; margin-bottom: -5px;"></div></a> </li>';
                 }
                 else {
-
-                    var currentVariant = productVariantsResult.filter(function (obj) {
-                        return (obj[2] == variantList[j][1]);
-                    });
-                    
                     variantBlock += '<li><a href="javascript:">' + variantList[j][0] + ': <span class="">' + currentVariant[0][4] + '</span></a></li>';
                 }
             }
+            variantBlock += '<li><a href="javascript:">Created Date: <span class="">' + cartObj[i].CreatedDate + '</span></a></li>';
             cartItemBlock = cartItemBlock.replace("@@VariantOptions", variantBlock);
         }
 
@@ -181,6 +180,16 @@ function clearCart() {
     localStorage.setItem("cart", '');
     $('#reviewCart').html('<tr><td  colspan="3">No Items..</td></tr>');
     updateCartCount();
+    $("#msg-container").removeClass('hide');
+    $("#msg").html('Cart cleared...')
+    window.setInterval(function () {
+        var timeLeft = $("#timeLeft").html();
+        if (eval(timeLeft) == 0) {
+            window.location.href = "products.html";
+        } else {
+            $("#timeLeft").html(eval(timeLeft) - eval(1));
+        }
+    }, 1000);
 }
 function getUserAgent() {
     var txt = navigator.userAgent;
@@ -196,4 +205,40 @@ function updateCartCount() {
     else {
         $('#cartItemCount').html('0');
     }
+}
+
+function navigateToProducts(Category) {
+    sessionStorage.setItem("selectedCategory", Category);
+    window.location.href = "products.html";
+}
+function submitOrder() {
+
+    $("#msg-container").removeClass('hide');
+    $("#msg").html('Submitted successfully...')
+    window.setInterval(function () {
+        var timeLeft = $("#timeLeft").html();
+        if (eval(timeLeft) == 0) {
+            window.location.href = "products.html";
+        } else {
+            $("#timeLeft").html(eval(timeLeft) - eval(1));
+        }
+    }, 1000);
+}
+function editCart(ProductID) {
+    sessionStorage.setItem('cartProductToEdit', ProductID);
+    window.location.href = "productdetails.html"
+}
+function deleteCartRow(cartRowIndex) {
+
+    if (confirm("Are you sure?")) {
+
+        var cartObj = JSON.parse(localStorage.getItem("cart"));
+        cartObj = cartObj.filter(function (obj) {
+            return obj.CartRowIndex != cartRowIndex;
+        })
+        localStorage.setItem("cart", JSON.stringify(cartObj));
+        loadReviewCart();
+        updateCartCount();
+    }
+
 }
