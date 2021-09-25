2ef3b58cf4f9ec38c45a6287a79bc32eea456bcd
IFC2021 <88974538+IFC2021@users.noreply.github.com>
2021-09-25 18:55:05 +0530

    Changes for 1.22 (Main)

--- js/index.js
+++ js/index.js
@@ -1,49 +1,30 @@
 
-var sheetAPIBaseURL = "https://sheets.googleapis.com/v4/spreadsheets"
-var ExcelfileID = "1QvHgvjouMiFIMZJoAdVPo6NHhLRQJlG_0mmn3B7Zc3E";
-var apiKey = "AIzaSyAmElMC6fNzymbUfgLF9_3r0GbIqwHjGyE";
-
-/*Read categories*/
-var categorySheetName = "Categories";
-var categoryFromRange = "A1";
-var categoryToRange = "A22";
-
-var readCategoryURL = sheetAPIBaseURL + "/" + ExcelfileID + "/values/" + categorySheetName + "!" + categoryFromRange + ":" + categoryToRange + "?key=" + apiKey;
-/*Read categories ends*/
-
-/*Read products*/
-var productSheetName = "Products";
-var productFromRange = "A1";
-var productToRange = "E22";
-var readProductURL = sheetAPIBaseURL + "/" + ExcelfileID + "/values/" + productSheetName + "!" + productFromRange + ":" + productToRange + "?key=" + apiKey;
-/*Read products ends*/
-
-/*Read products*/
-var productVariantsSheetName = "ProductVariants";
-var productVariantsFromRange = "A1";
-var productVariantsToRange = "F100";
-var readProductVariantsURL = sheetAPIBaseURL + "/" + ExcelfileID + "/values/" + productVariantsSheetName + "!" + productVariantsFromRange + ":" + productVariantsToRange + "?key=" + apiKey;
-/*Read products ends*/
-
-
-var categoryResult = null;
-var productResult = null;
-
 $(document).ready(function () {
+    /* fetches categories and products from local storage, 
+    if doesnt found, makes an ajax call and then loads Menu and featured products.*/
 
     categoryResult = JSON.parse(localStorage.getItem("categoryResult"));
     productResult = JSON.parse(localStorage.getItem("productResult"));
 
     if (categoryResult == null || categoryResult == '') {
         getCategoriesAjax();
-    }
-    else {
+        categoryResult = JSON.parse(localStorage.getItem("categoryResult"));
         loadMenuCategories(categoryResult);
         loadSearchCategories(categoryResult);
         loadMobileViewMenuCat(categoryResult);
     }
+    else {
+        categoryResult = JSON.parse(localStorage.getItem("categoryResult"));
+        loadMenuCategories(categoryResult);
+        loadSearchCategories(categoryResult);
+        loadMobileViewMenuCat(categoryResult);
+    }
+
     if (productResult == null || productResult == '') {
         getProductsAjax();
+        productResult = JSON.parse(localStorage.getItem("productResult"));
+        loadFeaturedProducts(productResult);
+
     }
     else {
         loadFeaturedProducts(productResult);
@@ -51,75 +32,18 @@ $(document).ready(function () {
     updateCartCount();
 });
 
-
-function getCategoriesAjax() {
-
-    $.ajax({
-        type: "GET",
-        url: readCategoryURL,
-        cache: false,
-        dataType: "json",
-        success: function (data) {
-            categoryResult = data.values.slice(1); //removed first row. it contains column title
-            localStorage.setItem("categoryResult", JSON.stringify(categoryResult));
-            loadMenuCategories(categoryResult);
-            loadSearchCategories(categoryResult);
-            loadMobileViewMenuCat(categoryResult);
-        }
-    });
-}
-function getProductsAjax() {
-
-    $.ajax({
-        type: "GET",
-        url: readProductURL,
-        cache: false,
-        dataType: "json",
-        success: function (data) {
-            productResult = data.values.slice(1); //removed first row. it contains column title
-            localStorage.setItem("productResult", JSON.stringify(productResult));
-            loadFeaturedProducts(productResult);
-        }
-    });
-
-}
-function loadMenuCategories(categories) {
-    var catPart1 = '<li><a href="javascript:" onclick="navigateToProducts(\'All\')" >All Categories</a></li>';
-    var catPart2 = '';
-    var partition = Math.round((categories.length / 2));
-
-    for (var i = 0; i < partition; i++) {
-        catPart1 += '<li><a href="javascript:" onclick="navigateToProducts(\'' + categories[i] + '\')"> ' + categories[i] + '</a ></li >';
-    }
-    for (var j = partition; j < categories.length; j++) {
-        catPart2 += '<li><a href="javascript:" onclick="navigateToProducts(\'' + categories[j] + '\')"> ' + categories[j] + '</a ></li >';
-    }
-    $('#catPart1').html(catPart1);
-    $('#catPart2').html(catPart2);
-}
-function loadSearchCategories(categories) {
-    var ddlOptions = "<option value=''>All Categories</option>";
-    for (var i = 0; i < categories.length; i++) {
-        ddlOptions += "<option value=''>" + categories[i] + "</option>";
-    }
-    $('#searchCat').html(ddlOptions);
-}
-function loadMobileViewMenuCat(categories) {
-    var mobileViewMenuCat = '<li><a href="javascript:" onclick="navigateToProducts(\'All\')" >All Categories</a></li>';
-    for (var i = 0; i < categories.length; i++) {
-        var encodedURL = encodeURIComponent(categories[i]);
-        mobileViewMenuCat += '<li><a href="javascript:" onclick="navigateToProducts(\'' + categories[i] + '\')">' + categories[i] + '</a></li>';
-        $('#mobileViewMenuCat').html(mobileViewMenuCat);
-    }
-}
+/*Loads featured products */
 function loadFeaturedProducts(products) {
+
+    /* Generate random numbers from 1 to product list length and shows 8 products randomly in featured products */
     var featuredProductBlock = '';
     for (var i = 0; i < 8; i++) {
         var product = products[Math.floor(Math.random() * products.length)];
-        featuredProductBlock += '<div class="product-default inner-quickview inner-icon"><figure><a onclick="navigateToProductDetails(\'' + product[1] + '\')\" href="javascript:"><img src="ProductImages/' + product[3] + '"></a><a href="javascript:" class="btn-quickview" onclick="quickView(\'' + product[1] + '\')"  title="Quick View">Quick View</a> </figure> <div class="product-details"> <div class="category-wrap"> <div class="category-list"> <a href="javascript:" class="product-category">' + product[0] + '</a> </div> </div> <h3 class="product-title"> <a href="productdetails.html">' + product[2] + '</a> </h3>  </div></div>';
+        featuredProductBlock += '<div class="product-default inner-quickview inner-icon"><figure><a onclick="navigateToProductDetails(\'' + product[1] + '\')\" href="javascript:"><img src="ProductImages/' + product[3] + '"></a>  </figure> <div class="product-details"> <div class="category-wrap"> <div class="category-list"> <a href="javascript:" class="product-category">' + product[0] + '</a> </div> </div> <h3 class="product-title"> <a href="productdetails.html">' + product[2] + '</a> </h3>  </div></div>';
     }
 
     $('#featuredProductBlock').html(featuredProductBlock);
+    /* After loading featured products HTML, need to re-initialize slider since the HTML and images are created newly in DOM */
 
     var newSliderOptions = {
         "loop": false,
@@ -147,39 +71,10 @@ function loadFeaturedProducts(products) {
     $('#featuredProductBlock').owlCarousel(newSliderOptions);
 
 }
-function quickView(productID) {
-    var product = productResult.filter(function (obj) {
-        return (obj[1] === productID)
-    });
-
-    $('#quickViewProductImage').attr('src', 'ProductImages/' + product[0][3]);
-    $('#quickViewProductName').html(product[0][2]);
-    $('#quickViewProductDescription').html(product[0][4]);
-    $('#quickView').modal('show');
-}
+/* Shows quick view on click of quick button on products */
+ 
+/*navigates to product details page on click of any product (featured products in this page) */
 function navigateToProductDetails(productID) {
     sessionStorage.setItem("selectedProductID", productID);
     window.location.href = "productdetails.html";
-}
-
-
-function getUserAgent() {
-    var txt = navigator.userAgent;
-    txt += "<br>Resolution: " + Math.round(window.screen.width) + "x" + Math.round(window.screen.height);
-    txt += "<br>Browser Online: " + navigator.onLine;
-    $('#userAgent').html(txt);
-}
-function updateCartCount() {
-    if (localStorage.getItem("cart") != null && localStorage.getItem("cart") != "") {
-        var cartObj = JSON.parse(localStorage.getItem("cart"));
-        $('#cartItemCount').html(cartObj.length);
-    }
-    else {
-        $('#cartItemCount').html('0');
-    }
-}
-
-function navigateToProducts(Category) {
-    sessionStorage.setItem("selectedCategory", Category);
-    window.location.href = "products.html";
 }
--- js/products.js
+++ js/products.js
@@ -1,43 +1,24 @@
-var sheetAPIBaseURL = "https://sheets.googleapis.com/v4/spreadsheets"
-var ExcelfileID = "1QvHgvjouMiFIMZJoAdVPo6NHhLRQJlG_0mmn3B7Zc3E";
-var apiKey = "AIzaSyAmElMC6fNzymbUfgLF9_3r0GbIqwHjGyE";
-
-/*Read categories*/
-var categorySheetName = "Categories";
-var categoryFromRange = "A1";
-var categoryToRange = "A22";
-
-var readCategoryURL = sheetAPIBaseURL + "/" + ExcelfileID + "/values/" + categorySheetName + "!" + categoryFromRange + ":" + categoryToRange + "?key=" + apiKey;
-/*Read categories ends*/
-
-/*Read products*/
-var productSheetName = "Products";
-var productFromRange = "A1";
-var productToRange = "E22";
-var readProductURL = sheetAPIBaseURL + "/" + ExcelfileID + "/values/" + productSheetName + "!" + productFromRange + ":" + productToRange + "?key=" + apiKey;
-/*Read products ends*/
-
-/*Read products*/
-var productVariantsSheetName = "ProductVariants";
-var productVariantsFromRange = "A1";
-var productVariantsToRange = "F100";
-var readProductVariantsURL = sheetAPIBaseURL + "/" + ExcelfileID + "/values/" + productVariantsSheetName + "!" + productVariantsFromRange + ":" + productVariantsToRange + "?key=" + apiKey;
-/*Read products ends*/
-
-
-var categoryResult = null;
-var productResult = null;
-var productVariantsResult = null;
-
 
 $(document).ready(function () {
-    //fetch categories and products from local storage
+
+    $("#quickBuyViewPopup").downupPopup({
+        distance: 20,
+        width: "98%"
+    });
+
+    /* fetches categories and products from local storage,
+    if doesnt found, makes an ajax call and then loads Menu and products.*/
+
     categoryResult = JSON.parse(localStorage.getItem("categoryResult"));
     productResult = JSON.parse(localStorage.getItem("productResult"));
-    productVariantsResult = JSON.parse(localStorage.getItem("productVariantsResult"));
 
     if (categoryResult == null || categoryResult == '') {
         getCategoriesAjax();
+        categoryResult = JSON.parse(localStorage.getItem("categoryResult"));
+        loadMenuCategories(categoryResult);
+        loadSearchCategories(categoryResult);
+        loadProductsSideBarCategories(categoryResult);
+        loadMobileViewMenuCat(categoryResult);
     }
     else {
         loadMenuCategories(categoryResult);
@@ -47,6 +28,8 @@ $(document).ready(function () {
     }
     if (productResult == null || productResult == '') {
         getProductsAjax();
+        productResult = JSON.parse(localStorage.getItem("productResult"));
+        loadProducts(productResult);
     }
     else {
         loadProducts(productResult);
@@ -55,75 +38,19 @@ $(document).ready(function () {
     $("#txtSearch").on("keyup", function () {
         var value = $(this).val().toLowerCase();
         $("#productBlock .col-6").filter(function () {
-            $(this).toggle($(this).find('.product-title a').text().toLowerCase().indexOf(value) > -1)
+            $(this).toggle(
+                ($(this).find('.product-title a').text().toLowerCase().indexOf(value) > -1 ||
+                    $(this).find('.category-list a').text().toLowerCase().indexOf(value) > -1)
+            )
         });
     });
     updateCartCount();
 });
 
-function getCategoriesAjax() {
-
-    $.ajax({
-        type: "GET",
-        url: readCategoryURL,
-        cache: false,
-        dataType: "json",
-        success: function (data) {
-            categoryResult = data.values.slice(1); //removed first row. it contains column title
-            localStorage.setItem("categoryResult", JSON.stringify(categoryResult));
-            loadMenuCategories(categoryResult);
-            loadSearchCategories(categoryResult);
-            loadProductsSideBarCategories(categoryResult);
-            loadMobileViewMenuCat(categoryResult);
-        }
-    });
-}
-function getProductsAjax() {
-
-    $.ajax({
-        type: "GET",
-        url: readProductURL,
-        cache: false,
-        dataType: "json",
-        success: function (data) {
-            productResult = data.values.slice(1); //removed first row. it contains column title
-            localStorage.setItem("productResult", JSON.stringify(productResult));
-            loadProducts(productResult);
-        }
-    });
-
-}
 function refreshData() {
     getCategoriesAjax();
     getProductsAjax();
-}
-function loadMenuCategories(categories) {
-    var catPart1 = '<li><a href="javascript:" onclick="navigateToProducts(\'All\')" >All Categories</a></li>';
-    var catPart2 = '';
-    var partition = Math.round((categories.length / 2));
-
-    for (var i = 0; i < partition; i++) {
-        catPart1 += '<li><a href="javascript:" onclick="navigateToProducts(\'' + categories[i] + '\')"> ' + categories[i] + '</a ></li >';
-    }
-    for (var j = partition; j < categories.length; j++) {
-        catPart2 += '<li><a href="javascript:" onclick="navigateToProducts(\'' + categories[j] + '\')"> ' + categories[j] + '</a ></li >';
-    }
-    $('#catPart1').html(catPart1);
-    $('#catPart2').html(catPart2);
-}
-function loadSearchCategories(categories) {
-    var ddlOptions = "<option value=''>All Categories</option>";
-    for (var i = 0; i < categories.length; i++) {
-        ddlOptions += "<option value=''>" + categories[i] + "</option>";
-    }
-    $('#searchCat').html(ddlOptions);
-}
-function loadMobileViewMenuCat(categories) {
-    var mobileViewMenuCat = '<li><a href="javascript:" onclick="navigateToProducts(\'All\')" >All Categories</a></li>';
-    for (var i = 0; i < categories.length; i++) {
-        mobileViewMenuCat += '<li><a href="javascript:" onclick="navigateToProducts(\'' + categories[i] + '\')">' + categories[i] + '</a></li>';
-        $('#mobileViewMenuCat').html(mobileViewMenuCat);
-    }
+    window.location.href = "products.html";
 }
 function loadProductsSideBarCategories(categories) {
     var sidebarCat = '<li><a href="javascript:" onclick="navigateToProducts(\'All\')" >All Categories</a></li>';
@@ -146,46 +73,20 @@ function loadProducts(products) {
     $('#breadcrumb-selected-category').html(selectedCategory);
     var productBlock = '';
     for (var i = 0; i < products.length; i++) {
-        productBlock += '<div class="col-6 col-sm-4"><div class="product-default inner-quickview inner-icon"><figure><a onclick="navigateToProductDetails(\'' + products[i][1] + '\')\" href="javascript:"><img src="ProductImages/' + products[i][3] + '"></a><a href="javascript:" class="btn-quickview" onclick="quickView(\'' + products[i][1] + '\')" title="Quick View">Quick View</a></figure><div class="product-details"><div class="category-wrap"><div class="category-list"><a href="javascript:" class="product-category">' + products[i][0] + '</a></div></div><h3 class="product-title"><a onclick="navigateToProductDetails("' + products[i][1] + '")" href="javascript:">' + products[i][2] + '</a></h3></div></div></div>';
-
+        productBlock += '<div class="col-6 col-sm-4"><div class="product-default inner-quickview inner-icon"><figure><a onclick="navigateToProductDetails(\'' + products[i][1] + '\')\" href="javascript:"><img src="ProductImages/' + products[i][3] + '"></a><div class="btn-icon-group"><button class="btn-icon-buy btn btn-info" id="btnQuickBuy" onclick="showQuickBuy(\'' + products[i][1] + '\')" >Buy</button></div> </figure><div class="product-details"><div class="category-wrap">  <div class="category-list"><a href="javascript:" class="product-category">' + products[i][0] + '</a></div> </div><h3 class="product-title"><a onclick="navigateToProductDetails(\'' + products[i][1] + '\')" href="javascript:">' + products[i][2] + '</a></h3> </div>  </div></div>';
     }
     $('#productBlock').html(productBlock);
 }
-function quickView(productID) {
-    var product = productResult.filter(function (obj) {
-        return (obj[1] === productID)
-    });
 
-    $('#quickViewProductImage').attr('src', 'ProductImages/' + product[0][3]);
-    $('#quickViewProductName').html(product[0][2]);
-    $('#quickViewProductDescription').html(product[0][4]);
-    $('#quickView').modal('show');
+function showQuickBuy(productID) {
+    sessionStorage.setItem("selectedProductID", productID);
+    $('#quickBuyViewPopup').downupPopup('open');
+    var iframe = document.getElementById("quickBuyView");
+    iframe.src = iframe.src;
 }
+
 function navigateToProductDetails(productID) {
     sessionStorage.setItem("selectedProductID", productID);
     window.location.href = "productdetails.html";
 }
-function clearCart() {
-    localStorage.setItem("cart", '');
-    $('#reviewCart').html('<tr><td  colspan="3">No Items..</td></tr>');
-}
-function getUserAgent() {
-    var txt = navigator.userAgent;
-    txt += "<br>Resolution: " + Math.round(window.screen.width) + "x" + Math.round(window.screen.height);
-    txt += "<br>Browser Online: " + navigator.onLine;
-    $('#userAgent').html(txt);
-}
-function updateCartCount() {
-    if (localStorage.getItem("cart") != null && localStorage.getItem("cart") != "") {
-        var cartObj = JSON.parse(localStorage.getItem("cart"));
-        $('#cartItemCount').html(cartObj.length);
-    }
-    else {
-        $('#cartItemCount').html('0');
-    }
-}
 
-function navigateToProducts(Category) {
-    sessionStorage.setItem("selectedCategory", Category);
-    window.location.href = "products.html";
-}
