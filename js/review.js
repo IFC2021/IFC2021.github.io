c0d4cbc7eeb0365d6fbc8295b2a33a951a621b60
IFC2021 <88974538+IFC2021@users.noreply.github.com>
2022-08-09 21:38:58 +0530

    Update review.js

--- js/review.js
+++ js/review.js
@@ -37,6 +37,7 @@ $(document).ready(function () {
     }
     updateCartCount();
     var mailObj = mailUtil();
+    mailObj.Initialize();
 });
 //------------------------------------------------------------------------
 /*function to load review cart */
